ea31e555ed03fb887f5512bfed5da287e7523bc2
teacher <teacher@BE-403->
2023-07-10 20:45:13 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/entity/Customer.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/entity/Customer.java
@@ -1,10 +1,15 @@
 package com.example.demo.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 @Entity
@@ -18,5 +23,8 @@ public class Customer {
 	@Column
 	private String name; // 客戶名稱
 	
+	@OneToMany(mappedBy = "customer")
+	@OrderBy("id ASC")
+	private Set<Order> orders = new LinkedHashSet<>(); // 訂單
 	
 }
