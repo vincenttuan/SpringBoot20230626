94eb34ad2c98fe5f3ee3501dd5fe6eeb7b73c2c9
teacher <teacher@BE-403->
2023-07-10 19:33:03 +0800

    psi

---
+++ SpringBoot-PSI/src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 客戶序號
+	
+	@Column
+	private String name; // 客戶名稱
+	
+	
+}
