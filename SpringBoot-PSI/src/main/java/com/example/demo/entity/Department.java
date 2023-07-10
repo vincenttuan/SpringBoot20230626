8d55d101e00ddba2060ec547e9178aa9e4a2abf7
teacher <teacher@BE-403->
2023-07-10 19:40:32 +0800

    psi

---
+++ SpringBoot-PSI/src/main/java/com/example/demo/entity/Department.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "department")
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 部門序號 
+	
+	@Column
+	private String name; // 部門名稱
+	
+}
