f1b1cf9a48c329ea146942a86b86ff681837f373
teacher <teacher@BE-403->
2023-07-10 20:41:36 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/entity/Product.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/entity/Product.java
@@ -1,10 +1,15 @@
 package com.example.demo.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 @Entity
@@ -24,4 +29,7 @@ public class Product {
 	@Column
 	private Integer price; // 定價/售價
 	
+	@OneToMany(mappedBy = "product")
+	@OrderBy("id ASC")
+	private Set<PurchaseItem> purchaseItems = new LinkedHashSet<>(); // 採購單明細
 }
