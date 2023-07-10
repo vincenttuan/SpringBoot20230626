f802e089a4b321b3cbbea4ae400b4f4f94304012
teacher <teacher@BE-403->
2023-07-10 20:49:56 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/entity/OrderItem.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/entity/OrderItem.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,4 +19,8 @@ public class OrderItem {
 	
 	@Column
 	private Integer amount; // 訂購數量
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order; // 訂單
 }
