f7e926e353ff736153a419f08988783f56b22acb
teacher <teacher@BE-403->
2023-07-10 19:54:52 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/entity/Department.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/entity/Department.java
@@ -1,10 +1,15 @@
 package com.example.demo.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 @Entity
@@ -17,5 +22,9 @@ public class Department {
 	
 	@Column
 	private String name; // 部門名稱
+	
+	@OneToMany
+	@OrderBy("id ASC")
+	private Set<Employee> employees = new LinkedHashSet<>();
 	
 }
