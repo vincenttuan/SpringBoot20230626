81cca97be9b1ae7f4cf89d932c31b1959cbaabd2
teacher <teacher@BE-403->
2023-07-14 21:39:26 +0800

    jpa

--- SpringBoot-PSI/src/test/java/com/example/demo/testing/QueryOrder.java
+++ SpringBoot-PSI/src/test/java/com/example/demo/testing/QueryOrder.java
@@ -21,9 +21,16 @@ public class QueryOrder {
 		// 訂單id, 客戶姓名, 員工姓名, 總價, (主檔)
 		// 明細id, 商品名稱, 數量, 單價, 小計 (明細)
 		List<Order> orders = orderRepository.findAll();
+		
 		orders.forEach(order -> {
-			System.out.printf("訂單ID: %d 客戶: %s 員工: %s\n", 
-					order.getId(), order.getCustomer().getName(), order.getEmployee().getName());
+			// 計算該筆訂單的總價
+			int total = order.getOrderItems()
+	  				  .stream()
+					  .mapToInt(item -> item.getAmount() * item.getProduct().getPrice())
+					  .sum();
+			
+			System.out.printf("訂單ID: %d 客戶: %s 員工: %s 總價: %d\n", 
+					order.getId(), order.getCustomer().getName(), order.getEmployee().getName(), total);
 			System.out.println("訂單明細:");
 			order.getOrderItems().forEach(item -> {
 				System.out.printf("明細ID: %d 商品名稱: %s 數量: %d 單價: %d 小計: %d\n",
@@ -33,6 +40,15 @@ public class QueryOrder {
 			});
 			System.out.println("------------------------------------------------");
 		});
+		
+		// 所有訂單的總價
+		int sumTotal = orders.stream()
+				  .mapToInt(order -> order.getOrderItems()
+						  				  .stream()
+										  .mapToInt(item -> item.getAmount() * item.getProduct().getPrice())
+										  .sum())
+				  .sum(); 
+		System.out.printf("所有訂單的總價: %d\n", sumTotal);
 		
 	}
 }
