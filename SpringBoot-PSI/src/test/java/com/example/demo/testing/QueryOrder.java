f173becd54f60af8c0a0200ef4a1a4fa8d6bf6b3
teacher <teacher@BE-403->
2023-07-14 21:31:04 +0800

    jpa

--- SpringBoot-PSI/src/test/java/com/example/demo/testing/QueryOrder.java
+++ SpringBoot-PSI/src/test/java/com/example/demo/testing/QueryOrder.java
@@ -1,9 +1,12 @@
 package com.example.demo.testing;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.entity.Order;
 import com.example.demo.repository.OrderRepository;
 
 @SpringBootTest
@@ -17,6 +20,19 @@ public class QueryOrder {
 		// 查詢所有訂單與明細
 		// 訂單id, 客戶姓名, 員工姓名, 總價, (主檔)
 		// 明細id, 商品名稱, 數量, 單價, 小計 (明細)
+		List<Order> orders = orderRepository.findAll();
+		orders.forEach(order -> {
+			System.out.printf("訂單ID: %d 客戶: %s 員工: %s\n", 
+					order.getId(), order.getCustomer().getName(), order.getEmployee().getName());
+			System.out.println("訂單明細:");
+			order.getOrderItems().forEach(item -> {
+				System.out.printf("明細ID: %d 商品名稱: %s 數量: %d 單價: %d 小計: %d\n",
+						item.getId(), item.getProduct().getName(), item.getAmount(),
+						item.getProduct().getPrice(),
+						(item.getProduct().getPrice() * item.getAmount()));
+			});
+			System.out.println("------------------------------------------------");
+		});
 		
 	}
 }
