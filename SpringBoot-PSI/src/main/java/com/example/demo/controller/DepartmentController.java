a9794aa88a81e0eddb25b790fdfc069cd7c9ea08
teacher <teacher@BE-403->
2023-07-17 19:34:41 +0800

    psi

---
+++ SpringBoot-PSI/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
+
+@Controller
+@RequestMapping("/department")
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	@GetMapping("/")
+	public String index(@ModelAttribute Department department, Model model) {
+		model.addAttribute("departments", departmentRepository.findAll());
+		return "department";
+	}
+	
+}
