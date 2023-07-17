96386e24ce32b15cd64c8f6883dbd332f4796352
teacher <teacher@BE-403->
2023-07-17 20:52:25 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/controller/DepartmentController.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Department;
@@ -38,4 +39,10 @@ public class DepartmentController {
 		return "redirect:/department/";
 	}
 	
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") Long id, Department department) {
+		department.setId(id);
+		departmentRepository.save(department);
+		return "redirect:/department/";
+	}
 }
