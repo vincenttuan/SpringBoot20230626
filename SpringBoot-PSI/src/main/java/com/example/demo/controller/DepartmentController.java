0ae1d3bf2ce791ef461a8ce5b4d389fff742f94e
teacher <teacher@BE-403->
2023-07-17 19:56:41 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/controller/DepartmentController.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Department;
@@ -21,6 +22,12 @@ public class DepartmentController {
 	public String index(@ModelAttribute Department department, Model model) {
 		model.addAttribute("departments", departmentRepository.findAll());
 		return "department";
+	}
+	
+	@PostMapping("/")
+	public String create(Department department) {
+		departmentRepository.save(department);
+		return "redirect:/department/";
 	}
 	
 }
