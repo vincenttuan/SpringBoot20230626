f6f13d5d0dce13f524af06ef89468b3c6e75ae53
teacher <teacher@BE-403->
2023-07-24 19:59:34 +0800

    psi

--- SpringBoot-PSI/src/main/java/com/example/demo/repository/ProductRepository.java
+++ SpringBoot-PSI/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -12,12 +12,16 @@ import com.example.demo.entity.view.ProductSales;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	// 查詢所有商品的採購數量(purchase_item)與銷售數量(order_item)
+	// SQL 別名 amount1: 採購數量, amount2: 銷售數量
 	@Query(nativeQuery = true, value = "select p.id, p.name, p.cost, p.price, "
 			+ "(select sum(amount) from purchase_item where product_id = p.id limit 1) as amount1, "
 			+ "(select sum(amount) from order_item where product_id = p.id limit 1) as amount2 "
 			+ "from product p")
 	List<Inventory> queryInventory();
 	
+	// 查詢某商品(根據商品 id)的採購數量(purchase_item)與銷售數量(order_item)
+	// SQL 別名 amount1: 採購數量, amount2: 銷售數量
 	@Query(nativeQuery = true, value = "select p.id, p.name, p.cost, p.price, "
 			+ "(select sum(amount) from purchase_item where product_id = p.id limit 1) as amount1, "
 			+ "(select sum(amount) from order_item where product_id = p.id limit 1) as amount2 "
